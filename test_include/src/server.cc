d4f20e268c3b40cf079f8aa09cbaadc56b6f9212
Anders Busch <andersbusch@gmail.com>
2023-12-17 20:10:13 +0100

    fixing the server example to handle signals

--- test_include/src/server.cc
+++ test_include/src/server.cc
@@ -1,12 +1,31 @@
 #include "nettools/UnixServerSocket.hh"
+#include <signal.h>
+#include <unistd.h>
+#include <atomic>
+
+std::atomic<bool> want_to_quit(false);
+
+void got_signal(int) {
+    want_to_quit.store(true);
+}
 
 int main() { 
-    nettools::UnixServerSocket socket("/tmp/testsock.sock");
+
+    struct sigaction sa = { 0 };
+    sa.sa_handler = got_signal;
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, nullptr);
+    sigaction(SIGTERM, &sa, nullptr);
+
+    auto socket = nettools::UnixServerSocket("/tmp/testsock.sock");
 
     for (;;) {
         auto data_socket = socket.accept();
         if (data_socket) {
             data_socket->write("Hello there!");
+        }
+        if (want_to_quit.load()) {
+            break;
         }
     }
 }
