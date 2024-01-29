8ff17f4c08b6bdd3d97903ef36f4e0d87f6e3135
Anders Busch <andersbusch@gmail.com>
2024-01-29 20:31:45 +0100

    acceptor

--- asio_unix_socket/src/server.cc
+++ asio_unix_socket/src/server.cc
@@ -2,34 +2,56 @@
 #include <asio.hpp>
 #include <string>
 
+using net_protocol = asio::local::seq_packet_protocol;
+
+
+class Acceptor {
+public:
+    Acceptor(asio::io_context &io, net_protocol::endpoint const& endpoint) : m_io(io), m_acceptor(io, endpoint) {
+    }
+
+    void start() {
+        m_acceptor.async_accept([this](asio::error_code ec, net_protocol::socket s) {
+            accept(ec, std::move(s));
+        });
+    }
+
+    void accept(asio::error_code ec, net_protocol::socket socket) {
+        if (ec) {
+            std::cerr << "error stuff" << ec << std::endl;
+            return;
+        }
+        asio::error_code ecc;
+        std::size_t ss = socket.send(asio::buffer("Hello there"), 0, ecc);
+        if (!ecc) {
+            std::cout << "wrote --> " << ss << std::endl;
+        } else {
+            std::cerr << "error code " << ec << std::endl;
+        }
+        m_acceptor.async_accept([this](asio::error_code ec, net_protocol::socket s) {
+            accept(ec, std::move(s));
+        });
+    }
+
+private:
+    asio::io_context &m_io;
+    net_protocol::acceptor m_acceptor;
+};
+
+    
+
 int main() 
 {
     try
     {
-        std::string data = "Hello there";
         asio::io_context io;
 
-        asio::local::seq_packet_protocol::endpoint endpoint("/tmp/draw.sock");
+        net_protocol::endpoint endpoint("/tmp/draw.sock");
         
-        asio::local::seq_packet_protocol::acceptor acceptor(io, endpoint);
+        Acceptor acc(io, endpoint);
 
-        acceptor.async_accept([&data](std::error_code ec, asio::local::seq_packet_protocol::socket socket) {
-            if (ec) {
-                std::cerr << "error stuff" << ec << std::endl;
-                return;
-            }
-            std::cout << "Hello there!\n";
-            asio::error_code ecc;
-            std::size_t ss = socket.send(asio::buffer(data), 0, ecc);
-            if (!ecc) {
-                std::cout << "wrote --> " << ss << std::endl;
-            } else {
-                std::cerr << "error code " << ec << std::endl;
-            }
+        acc.start();
 
-            socket.close();
-        }); 
-        
         io.run();
     }
     catch (std::exception &e)
